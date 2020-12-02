1e60ed40f8b9d925904cba16235efa12c57773ec
betelgeuse-7 <abidindrd@gmail.com>
2020-12-02 16:47:22 +0300

    allowed hosts

--- contribauth/contribauth/settings.py
+++ contribauth/contribauth/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'fmxz*w90ridz2k^reg(5j#2cw!^w50k*=)$7lz+mfup7_58fzo'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['betelgeuse7.pythonanywhere.com']
 
 
 # Application definition
