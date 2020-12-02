2dc462b3a952cd413923dff7aa259a8fe9c59945
betelgeuse-7 <abidindrd@gmail.com>
2020-12-02 16:11:41 +0300

    little change

--- contribauth/contribauth/settings.py
+++ contribauth/contribauth/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
+SECRET_KEY = 'fmxz*w90ridz2k^reg(5j#2cw!^w50k*=)$7lz+mfup7_58fzo'
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
